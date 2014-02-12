6d4bd9e0894ba0e4b413193352aecc1d67131d2c
Gabriel Koerich <gabriel@algorit.com.br>
2014-02-12 10:11:32 -0200

    Create syncs migration

---
+++ src/migrations/2013_08_03_212645_create_syncs_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSyncsTable extends Migration
+{
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('syncs', function($table)
+		{
+
+			$table->increments('id');
+
+			$table->integer('morph_id')	 ->unsigned();
+			$table->integer('morph_type')->unsigned();
+
+			$table->string('entity')->nullable();
+			$table->string('type', 60);
+			$table->string('class');
+			$table->string('status', 10)->default('fail'); // fail / success
+			$table->text('response')->nullable();
+
+			$table->timestamp('started_at');
+			$table->timestamps();
+
+			$table->engine = 'InnoDB';
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('syncs');
+	}
+
+}

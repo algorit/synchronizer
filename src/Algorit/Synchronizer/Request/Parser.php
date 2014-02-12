3660e1cb6e75ea410a17dc5b1ad16e59d17587c8
Gabriel Koerich <gabriel@algorit.com.br>
2014-02-12 18:12:50 -0200

    Create base parser and repository

---
+++ src/Algorit/Synchronizer/Request/Parser.php
@@ -0,0 +1,99 @@
+<?php namespace Algorit\Synchronizer\Request;
+
+use App;
+use Str;
+use Log;
+use Event;
+use Closure;
+use Algorit\Synchronizer\Request\Config;
+use Algorit\Synchronizer\Request\Exceptions\ParserException;
+use Algorit\Synchronizer\Request\Contracts\SystemParserInterface;
+
+class Parser {
+
+	use ConfigTrait;
+
+	/**
+	 * The repository instance
+	 *
+	 * @var \Services\Sync\Erps\
+	 */
+	protected $repository;
+
+	/**
+	 * The filesystem instance
+	 *
+	 * @var \Services\Sync\Erps\Filesystem
+	 */
+	protected $files;
+
+	/**
+	 * The config instance
+	 *
+	 * @var \Services\Sync\Config
+	 */
+	protected $config;
+
+	/**
+	 * Create a new instance.
+	 *
+	 * @param  \Repositories\   $repository
+	 * @param  \Repositories\   $files
+	 * @return instance
+	 */
+	public function __construct(Filesystem $files, $namespace)
+	{
+		$this->namespace = $namespace;
+		$this->files = $files;
+	}
+
+	/**
+	 * Set parser config
+	 *
+	 * @param  Config  $config
+	 * @return void
+	 */
+	public function setConfig(Config $config)
+	{
+		$this->config = $config;
+	}
+
+	/**
+	 * Call a parser instance and set the aliases.
+	 *
+	 * @param  \Repositories\Interfaces\  $name
+	 * @param  \Closure 				  $callback
+	 * @return instance
+	 */
+	public function call($name)
+	{
+		$class = 'Synchronizer\Systems\Jjw\Parsers\\' . $this->getFromEntityName($name);
+
+		Log::notice('Loading parser ' . $class);
+
+		$parser = App::make($class);
+
+		$aliases = array_get($this->config->aliases, $name);
+
+		$parser->setAliases($aliases);
+
+		return $parser;
+	}
+
+	/**
+	 * Get the repository name from the plural entity name.
+	 *
+	 * @param  $entityName
+	 * @return string
+	 */
+	private function getFromEntityName($name)
+	{
+		if( ! is_string($name))
+		{
+			throw new ParserException('Wrong name format');
+		}
+		
+		return ucfirst(strtolower(Str::singular($name)));
+	}
+
+}
---
+++ src/Algorit/Synchronizer/Request/Repository.php
@@ -0,0 +1,47 @@
+<?php namespace Algorit\Synchronizer\Request;
+
+use App;
+use Str;
+use Log;
+use Synchronizer\Exceptions\RepositoryException;
+
+class Repository {
+
+	public function __construct($namespace)
+	{
+		$this->namespace = $namespace;
+	}
+
+	/**
+	 * Call a repository instance.
+	 *
+	 * @param  \Repositories\Interfaces\  $repositoryInterface
+	 * @param  \Closure 				  $callback
+	 * @return instance
+	 */
+	public function call($entity)
+	{
+		$class = $this->namespace . '\\' . $this->getFromEntityName($entity);
+
+		Log::notice('Loading repository ' . $class);
+
+		return App::make($class);
+	}
+
+	/**
+	 * Get the repository name from the plural entity name.
+	 *
+	 * @param  $name
+	 * @return string
+	 */
+	private function getFromEntityName($name)
+	{
+		if( ! is_string($name))
+		{
+			throw new RepositoryException('Wrong name format');
+		}
+
+		return ucfirst(strtolower(Str::singular($name)));
+	}
+
+}

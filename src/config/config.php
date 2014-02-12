c3ecee9570e177e50ac8161eff34e561ebf38160
Gabriel Koerich <gabriel@algorit.com.br>
2014-02-12 18:16:08 -0200

    Config stuff

--- src/config/config.php
+++ src/config/config.php
@@ -2,47 +2,6 @@
 
 return array(
 
-    'prefix'  => '',
-
-    'storage' => function()
-    {
-        return App::make('Application\Storage\Contracts\SyncInterface');
-    },
-
-    'system'  => 'Synchronizer\\Systems',
-
-    'create'  => function($system, $resource, $entity, $type)
-    {
-        $company_id = null;
-        $representative_id = null;
-
-        $class = explode('\\', get_class($this->resource));
-
-        switch(end($class))
-        {   
-            case 'Erp':
-                $erp_id = $resource->id;
-            break;
-            case 'Company':
-                $company_id = $resource->id;
-                $erp_id     = $resource->erp->id;
-            break;
-            case 'Representative':
-                $representative_id = $resource->id;
-                $company_id        = $resource->company->id;
-                $erp_id            = $resource->company->erp->id;
-            break;
-        }
-
-        return array(
-            'erp_id'            => $erp_id,
-            'company_id'        => $company_id,
-            'representative_id' => $representative_id,
-            'entity'            => $entity,
-            'type'              => $type,
-            'class'             => get_class($system),
-            'status'            => 'processing',
-        );
-    },
+    
 
 );
